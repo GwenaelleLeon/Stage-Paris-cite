1657c417d9a3571053ee146dc09c9893ad5a6f6b
Gwenaelle Leon <gweni.leon@gmail.com>
2022-05-02 12:28:08 +0200

    ajout du filter

--- first_steps.py
+++ first_steps.py
@@ -1,15 +1,25 @@
 from io import open 
-from conllu import parse_incr, parse
+import conllu as co 
 from numpy import datetime_as_string 
 
 data_file_pud = open("es_pud-ud-test.conllu", "r", encoding="utf-8")
 data_file_ancora = open("es_ancora-ud-train.conllu", "r", encoding="utf-8")
 data_file_gsd = open("es_gsd-ud-train.conllu", "r", encoding="utf-8")
 
-sentences_generator_pud = parse_incr(data_file_pud) # Generator for list of sentences 
-sentences_generator_ancora = parse_incr(data_file_ancora) # Generator for list of sentences 
-sentences_generator_gsd = parse_incr(data_file_gsd) # Generator for list of sentences 
+sentences_generator_pud = co.parse_incr(data_file_pud) # Generator for list of sentences 
+sentences_generator_ancora = co.parse_incr(data_file_ancora)  
+sentences_generator_gsd = co.parse_incr(data_file_gsd)
 
 for tokenlist in sentences_generator_pud:
+    print(tokenlist.filter(feats__Gender = "Masc"))
+
+map(lambda tokenlist: tokenlist.filter(feats__Gender = "Masc" ),sentences_generator_pud)
+
+ 
+"""
+for tokenlist in sentences_generator_pud:
     for word in tokenlist:
-        print(word["feats"]['Gender'])
+        if word["feats"] != None and "Gender" in word["feats"] :
+            if word["feats"]['Gender'] == "Masc":
+"""
+
