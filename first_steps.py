f2a78fa5d503cd48c702fba14a5490855ed66073
Gwenaelle Leon <gweni.leon@gmail.com>
2022-05-10 14:38:04 +0200

    erase generators

--- first_steps.py
+++ first_steps.py
@@ -1,27 +1,27 @@
 from io import open 
-import conllu as co 
-from numpy import datetime_as_string 
+import conllu as co
 
-data_file_pud = open("es_pud-ud-test.conllu", "r", encoding="utf-8")
-data_file_ancora = open("es_ancora-ud-train.conllu", "r", encoding="utf-8")
-data_file_gsd = open("es_gsd-ud-train.conllu", "r", encoding="utf-8")
+def count_gender (sentence, gender) :
+    return len(sentence.filter(feats__Gender=gender))
 
-sentences_generator_pud = co.parse_incr(data_file_pud) # Generator for list of sentences 
-sentences_generator_ancora = co.parse_incr(data_file_ancora)  
-sentences_generator_gsd = co.parse_incr(data_file_gsd)
+def count_gender_restreint (sentence, gender) :
+    return len(sentence.filter(upos=lambda x: x=="NOUN"or x=="PRON" or x=="PROPN", feats__Gender=gender))
 
-for tokenlist in sentences_generator_pud:
-    print(tokenlist.filter(feats__Gender = "Masc"))
+data_sources = ["es_pud-ud-test.conllu", "es_ancora-ud-train.conllu", "es_gsd-ud-train.conllu"]
 
-map(lambda tokenlist: tokenlist.filter(feats__Gender = "Masc" ),sentences_generator_pud)
+def analyse_data(data_source):
+    with open(data_source, "r", encoding="utf-8") as data_file:
+        strings = data_file.read()
+        sentences = co.parse(strings)
+        masc_full = 0
+        fem_full = 0
+        [masc_full := masc_full + count_gender_restreint(sentence, "Masc") for sentence in sentences]
+        [fem_full := fem_full + count_gender_restreint(sentence, "Fem") for sentence in sentences]
+        return masc_full, fem_full
 
 
-#Questo è un test per vedere se funziona github
- 
-"""
-for tokenlist in sentences_generator_pud:
-    for word in tokenlist:
-        if word["feats"] != None and "Gender" in word["feats"] :
-            if word["feats"]['Gender'] == "Masc":
-"""
+for data_source in data_sources:
+     genders = analyse_data(data_source)
+     print(data_source, genders[0], " masc, ", genders[1], " fem.")
+
 
