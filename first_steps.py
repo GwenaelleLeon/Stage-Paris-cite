6e38b6635a857308738d7156a5d1dac472a33054
Gwenaelle Leon <gweni.leon@gmail.com>
2022-05-11 10:02:59 +0200

    generalisation du code

--- first_steps.py
+++ first_steps.py
@@ -1,27 +1,40 @@
 from io import open 
 import conllu as co
 
-def count_gender (sentence, gender) :
-    return len(sentence.filter(feats__Gender=gender))
+def count_genders(sentences, tag): #Pour filtrer dans "upos"
+    masc_full = 0
+    fem_full = 0
+    for sentence in sentences:
+        number_masc = len(sentence.filter(upos=tag, feats__Gender="Masc"))
+        number_fem = len(sentence.filter(upos=tag, feats__Gender="Fem"))
+        masc_full += number_masc
+        fem_full += number_fem
+    return masc_full, fem_full
 
-def count_gender_restreint (sentence, gender) :
-    return len(sentence.filter(upos=lambda x: x=="NOUN"or x=="PRON" or x=="PROPN", feats__Gender=gender))
+def count_gender_pronoun(sentence, gender):
+     return len(sentence.filter(upos="PRON", feats__Gender=gender))
 
-data_sources = ["es_pud-ud-test.conllu", "es_ancora-ud-train.conllu", "es_gsd-ud-train.conllu"]
+data_sources = ["es_pud-ud-test.conllu"] #"es_ancora-ud-all.conllu", "es_gsd-ud-all.conllu"
 
-def analyse_data(data_source):
-    with open(data_source, "r", encoding="utf-8") as data_file:
-        strings = data_file.read()
-        sentences = co.parse(strings)
-        masc_full = 0
-        fem_full = 0
-        [masc_full := masc_full + count_gender_restreint(sentence, "Masc") for sentence in sentences]
-        [fem_full := fem_full + count_gender_restreint(sentence, "Fem") for sentence in sentences]
-        return masc_full, fem_full
+if __name__ ==  "__main__":
+    for data_source in data_sources:
+        with open(data_source, "r", encoding="utf-8") as data_file:
+            strings = data_file.read()
+            sentences = co.parse(strings)
+            print("Data source: ", data_source)
+            tags = {"Noun or Proper noun" : lambda x: x=="NOUN" or x=="PROPN", "Pronoun" : "PRON"}
+            for name, tag in tags:
+                masc, fem = count_genders(sentences, tag)
+                total = masc + fem
+                print("Tag:", name)
+                print("Masculine = ", masc)
+                print("Feminine =", fem)
+                print("Total: ", total)
+
+                
 
 
-for data_source in data_sources:
-     genders = analyse_data(data_source)
-     print(data_source, genders[0], " masc, ", genders[1], " fem.")
+
+
 
 
