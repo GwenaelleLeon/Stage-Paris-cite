ddd848d6e2af1ce22ae5f0487d48d4113299d2c1
Gwenaelle Leon <gweni.leon@gmail.com>
2022-05-12 13:44:09 +0200

    generalisation des tags

--- first_steps.py
+++ first_steps.py
@@ -5,36 +5,30 @@ def count_genders(sentences, tag): #Pour filtrer dans "upos"
     masc_full = 0
     fem_full = 0
     for sentence in sentences:
-        number_masc = len(sentence.filter(upos=tag, feats__Gender="Masc"))
-        number_fem = len(sentence.filter(upos=tag, feats__Gender="Fem"))
+        number_masc = len(sentence.filter(**tag, feats__Gender="Masc"))
+        number_fem = len(sentence.filter(**tag, feats__Gender="Fem"))
         masc_full += number_masc
         fem_full += number_fem
     return masc_full, fem_full
 
-def count_gender_pronoun(sentence, gender):
-     return len(sentence.filter(upos="PRON", feats__Gender=gender))
 
 data_sources = ["es_pud-ud-test.conllu"] #"es_ancora-ud-all.conllu", "es_gsd-ud-all.conllu"
 
 if __name__ ==  "__main__":
+    upos_nouns = {"upos" : lambda x: x =="NOUN" or x=="PROPN"} #Ici on definit le filtre qu'on veut et on le rajout aussi à tags
+    upos_pron = {"upos" : "PRON"}
+    deprel_nsubjpass = {"deprel" : "nsubj:pass"}
+    tags = {"Noun or Proper noun" : upos_nouns, "Pronoun" : upos_pron, "Deprel" : deprel_nsubjpass} 
     for data_source in data_sources:
         with open(data_source, "r", encoding="utf-8") as data_file:
             strings = data_file.read()
             sentences = co.parse(strings)
             print("Data source: ", data_source)
-            tags = {"Noun or Proper noun" : lambda x: x=="NOUN" or x=="PROPN", "Pronoun" : "PRON"}
-            for name in tags:
-                masc, fem = count_genders(sentences, tags[name])
+            for name, tag in tags.items():
+                masc, fem = count_genders(sentences, tag)
                 total = masc + fem
                 print("Tag:", name)
                 print("Masculine = ", masc, masc/total*100, "%")
                 print("Feminine =", fem,  fem/total*100, "%")
                 print("Total: ", total)
-
                 
-__name__
-
-
-
-
-
